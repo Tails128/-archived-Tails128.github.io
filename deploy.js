0d45c0dd565f98d82e476122f663156d7017471d
tails128 <defendifilippo@gmail.com>
2020-05-21 02:00:26 +0200

    fixed automatic deploy

--- deploy.js
+++ deploy.js
@@ -4,7 +4,7 @@ ghpages.publish(
   "__sapper__/export",
   {
     branch: "master",
-    repo: github.com / Tails128 / tails128.github.io,
+    repo: "https://github.com/Tails128/tails128.github.io",
     user: {
       name: "tails128",
       email: "defendifilippo@gmail.com",
